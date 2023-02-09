768820c962a6a226d1ba324a5d82f47355cd5bfc
ssamarin <semenssamarin@mail.ru>
2023-02-09 16:33:52 +0900

    add babel and polyfills

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,34 @@
 'use strict';
 
-let path = require('path'); 
+let path = require('path');
 
-module.exports = { 
-  mode: 'development',
-  entry: './js/script.js', 
+module.exports = {
+  mode: 'production',
+  entry: './js/script.js',
   output: {
-    filename: 'bundle.js', 
-    path: __dirname + '/js' 
+    filename: 'bundle.js',
+    path: __dirname + '/js'
   },
-  watch: true, 
+  watch: true,
 
-  devtool: "source-map", 
+  devtool: "source-map",
 
-  module: {} 
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', {
+                debug: true,
+                corejs: 3,
+                useBuiltIns: "usage"
+            }]]
+          }
+        }
+      }
+    ]
+  }
 };
